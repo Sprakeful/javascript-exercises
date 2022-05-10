0bea9608421997eb9d4517f463848de9959b0c64
Bence Kovacs <bencekovacs.ev@gmail.com>
2022-05-10 21:00:51 +0200

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+ removeFromArray = function(inputArray, arg) {
+    let args = Array.from(arguments);
+    for(let i = 0; i < args.length; i++){
+        if(i != 0){
+            let index = inputArray.indexOf(args[i]);
+            if(index != -1){
+                let dumpedArray = inputArray.splice(index, 1);
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
