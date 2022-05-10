a0c2f8cb18755fce562cc95cc3ca07145dbb2a89
Bence Kovacs <bencekovacs.ev@gmail.com>
2022-05-10 19:28:38 +0200

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let resultString = '';
+    if(num >= 0){
+        for(let i = 0; i < num; i++){
+            resultString += string;
+        }
+        return resultString;
+    }
+    else return 'ERROR';
 };
 
 // Do not edit below this line
