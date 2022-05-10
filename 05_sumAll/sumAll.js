0d180e518f7aae02f80a2df1e4c0174d5dcf301b
Bence Kovacs <bencekovacs.ev@gmail.com>
2022-05-10 21:18:37 +0200

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+    let result = 0;
+    let min = 0;
+    let max = 0;
+    if(val1 < 0 || val2 < 0 || typeof(val1) != 'number' || typeof(val2) != 'number') return 'ERROR';
+    if(val1 > val2){
+        min = val2;
+        max = val1;
+    }
+    else{
+        min = val1;
+        max = val2;
+    }
+    for(let i = min; i <= max; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
