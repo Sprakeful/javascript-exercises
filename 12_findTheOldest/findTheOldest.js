750ee723376eba59631bf2c6085a871a119dddb4
Bence Kovacs <bencekovacs.ev@gmail.com>
2022-05-27 21:23:28 +0200

    Complete exercises for TOP

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people.reduce((lastPerson, nextPerson) => {
+        let nextYOD = 0;
+        let lastYOD = 0;
+        if(!lastPerson.hasOwnProperty('yearOfDeath')) lastYOD = new Date().getFullYear();
+        else lastYOD = lastPerson.yearOfDeath;
+        
+        if(!nextPerson.hasOwnProperty('yearOfDeath')) nextYOD = new Date().getFullYear();
+        else nextYOD = nextPerson.yearOfDeath;
 
+        if((lastYOD - lastPerson.yearOfBirth) > (nextYOD - nextPerson.yearOfBirth)) return lastPerson;
+        else return nextPerson;
+    });
+    return oldestPerson
 };
 
 // Do not edit below this line
